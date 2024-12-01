c843b9b83819c8239e8440cc2fad6e84646fe034
youngjaychoi <youngjaychoi02@gmail.com>
2024-11-30 17:37:15 -0800

    commit2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
 
 int main() {
-    
+    IntBinaryTree tree;
+    ifstream inFile("codes.txt");
+    string code;
+
+    if (!inFile.open()) {
+        cout << "file not opened." << endl;
+        return 1;
+    }
 
 
-    
+
+
+
+
 
     return 0;
 }
